154aa988362b160a20292a72b5bc6905a971f505
hi-rola <rolaalbaa@gmail.com>
2021-02-12 17:12:18 -0600

    Se agrega funte y añaden componentes de angular material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialDesignModule } from './models/MaterialDesing.module';
+
+//componentes
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/MaterialDesing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const materialModules = [
+    MatButtonModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule, MatDividerModule,
+    MatRippleModule, MatDialogModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule,
+    MatSelectModule, MatTooltipModule, MatDatepickerModule, MatNativeDateModule, MatSnackBarModule
+];
+
+@NgModule({
+    imports: [
+        ...materialModules
+    ],
+    exports: [
+        ...materialModules
+    ],
+})
+export class MaterialDesignModule { }
