164285e3663b6e727e837b5e792fd4098c6db6dc
hi-rola <rolaalbaa@gmail.com>
2021-02-17 18:41:23 -0600

    Diseño header.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  nombreMaestro: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getNombre();
+  }
+
+  getNombre(): string {
+    return this.nombreMaestro = localStorage.getItem('nombre') + ' ' + localStorage.getItem('apellidos');
   }
 
 }
