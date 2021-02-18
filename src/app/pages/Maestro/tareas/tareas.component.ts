c83621afb6f9d5a5e3ab02664ff4249efcfca73e
hi-rola <rolaalbaa@gmail.com>
2021-02-17 18:42:47 -0600

    Se agrega la funcionalidad de registrar tarea.

---
+++ src/app/pages/Maestro/tareas/tareas.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { MaestroService } from '../maestro.service';
+import { Tarea } from '../Tarea';
+import { RegistrarTareaComponent } from './registrar-tarea/registrar-tarea.component';
+import { MsjEliminarTareaComponent } from './msj-eliminar-tarea/msj-eliminar-tarea.component';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.scss']
+})
+export class TareasComponent implements OnInit {
+  centered = false;
+  listTareas: Tarea[] = [];
+  horizontalPosition: MatSnackBarHorizontalPosition = 'left';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  constructor(private maestroService: MaestroService, public dialog: MatDialog, private _snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.getTareasByMaestro();
+  }
+
+  getTareasByMaestro() {
+    let id_mestro = this.getId();
+    this.maestroService.getTareasByMaestro(id_mestro).subscribe(
+      (result: Tarea[]) => {
+        this.listTareas = result;
+      }
+    )
+  }
+
+  abrirDialog() {
+    const dialogRef = this.dialog.open(RegistrarTareaComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.ngOnInit();
+        this.getTareasByMaestro();
+      }
+    });
+  }
+
+  dialogEliminarTarea(tarea: Tarea) {
+    const dialogRef = this.dialog.open(MsjEliminarTareaComponent, {
+      width: '450px',
+      data: { nombre: tarea.nombre }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.maestroService.eliminarTarea(tarea.id_tarea).subscribe(
+          result => {
+            this.ngOnInit();
+            this._snackBar.open('Tarea eliminada exitosamente', '', {
+              duration: 2000,
+              panelClass: 'error-alert-snackbar',
+              horizontalPosition: this.horizontalPosition,
+              verticalPosition: this.verticalPosition,
+            });
+          },
+          error => {
+            this._snackBar.open('Problemas al eliminar la tarea, por favor inténtelo más tarde', '', {
+              duration: 2000,
+              panelClass: 'error-alert-snackbar',
+              horizontalPosition: this.horizontalPosition,
+              verticalPosition: this.verticalPosition,
+            });
+          }
+        );
+      }
+    });
+  }
+
+  getId(): string {
+    return localStorage.getItem('idM');
+  }
+
+
+}
