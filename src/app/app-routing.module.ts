711453c9d1168ecc0c8fdb0583077929492fc599
hi-rola <rolaalbaa@gmail.com>
2021-02-19 19:39:21 -0600

    Se agregan rutas y componentes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/Maestro/home/home.component';
 import { TareasComponent } from './pages/Maestro/tareas/tareas.component';
 import { LoginComponent } from './shared/login/login.component';
+import { EstadisticasTareaComponent } from './pages/Maestro/tareas/estadisticas-tarea/estadisticas-tarea.component';
+import { EstudiantesMaestroComponent } from './pages/Maestro/estudiantes-maestro/estudiantes-maestro/estudiantes-maestro.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home-academico', component: HomeComponent },
-  { path: 'tareas-academico', component: TareasComponent }
+  { path: 'tareas-academico', component: TareasComponent },
+  { path: 'estadisticas-tarea', component: EstadisticasTareaComponent },
+  { path: 'estudiantes-maestros', component: EstudiantesMaestroComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialDesignModule } from './models/MaterialDesing.module';
 
+import { ChartsModule } from 'ng2-charts';
+
 //componentes
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/Maestro/home/home.component';
 import { TareasComponent } from './pages/Maestro/tareas/tareas.component';
-import { EstudiantesComponent } from './pages/Maestro/estudiantes/estudiantes.component';
 import { LoginComponent } from './shared/login/login.component';
 import { RegistrarTareaComponent } from './pages/Maestro/tareas/registrar-tarea/registrar-tarea.component';
 import { MsjEliminarTareaComponent } from './pages/Maestro/tareas/msj-eliminar-tarea/msj-eliminar-tarea.component';
+import { ActualizarTareaComponent } from './pages/Maestro/tareas/actualizar-tarea/actualizar-tarea.component';
+import { EstadisticasTareaComponent } from './pages/Maestro/tareas/estadisticas-tarea/estadisticas-tarea.component';
+import { EstudiantesMaestroComponent } from './pages/Maestro/estudiantes-maestro/estudiantes-maestro/estudiantes-maestro.component';
+import { RegistrarEstudianteComponent } from './pages/Maestro/estudiantes-maestro/registrar-estudiante/registrar-estudiante.component';
 
 
 @NgModule({
@@ -26,20 +31,25 @@ import { MsjEliminarTareaComponent } from './pages/Maestro/tareas/msj-eliminar-t
     FooterComponent,
     HomeComponent,
     TareasComponent,
-    EstudiantesComponent,
     LoginComponent,
     RegistrarTareaComponent,
-    MsjEliminarTareaComponent
+    MsjEliminarTareaComponent,
+    ActualizarTareaComponent,
+    EstadisticasTareaComponent,
+    EstudiantesMaestroComponent,
+    RegistrarEstudianteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialDesignModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartsModule
   ],
   entryComponents: [
-    RegistrarTareaComponent, MsjEliminarTareaComponent
+    RegistrarTareaComponent, MsjEliminarTareaComponent, ActualizarTareaComponent,
+    RegistrarEstudianteComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
